1e52ac16cf645f97e80111790520db94aa8cfb9c
Simon Grchevski <simongrcevski@gmail.com>
2021-11-18 01:20:43 +0100

    Add few basic styles to reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,7 +2,6 @@ class ReservationsController < ApplicationController
   def destroy
     @reservation = Reservation.find(params[:id])
     @calendar = Calendar.find(@reservation.calendar_id)
-    @reservation.destroy
     @calendar.destroy
     flash[:notice] = "Reservation Canceled"
     redirect_to request.referrer
